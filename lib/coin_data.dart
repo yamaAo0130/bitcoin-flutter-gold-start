d81b6d8f9d8b07e9f77b221a3f67e5af8e82111a
yamaAo0130 <yamaguchiaoi0130@gmail.com>
2021-11-01 15:19:41 +0900

    金レベル

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,21 +32,27 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '8C27AAF3-EA29-4C67-A74A-3007003FE69A';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
     //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //TODO 5: Return a Map of the results instead of a single value.
-    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
-    http.Response response = await http.get(requestURL);
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      var lastPrice = decodedData['rate'];
-      return lastPrice;
-    } else {
-      print(response.statusCode);
-      throw 'Problem with the get request';
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      //TODO 5: Return a Map of the results instead of a single value.
+      String requestURL = '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        var lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        //Mapへ代入
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -51,17 +51,25 @@ class _PriceScreenState extends State<PriceScreen> {
       children: pickerItems,
     );
   }
-
-  String value = '?';
-
+//1BTS = ?USD
+  //String value = '?';
+//value had to be updated into a Map to store the values of all three cryptocurrencies.
+  Map<String, String> coinValues = {};
   //TODO 7: Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. Hint: You'll need a ternary operator.
-
+  bool isWaiting = false;
   //TODO 6: Update this method to receive a Map containing the crypto:price key value pairs. Then use that map to update the CryptoCards.
   void getData() async {
+    //7: Second, we set it to true when we initiate the request for prices.
+    isWaiting = true;
     try {
-      double data = await CoinData().getCoinData(selectedCurrency);
+      // getCoinDataの戻り値をmap型へ変更
+      var data = await CoinData().getCoinData(selectedCurrency);
+      //7. Third, as soon the above line of code completes, we now have the data and no longer need to wait. So we can set isWaiting to false.
+      isWaiting = false;
       setState(() {
-        value = data.toStringAsFixed(0);
+        //value = data.toStringAsFixed(0);
+        coinValues=data;
+
       });
     } catch (e) {
       print(e);
@@ -75,6 +83,22 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   //TODO: For bonus points, create a method that loops through the cryptoList and generates a CryptoCard for each.
+  Column makeCards() {
+   List<CryptoCard> cryptoCards = [];
+   for (String crypto in cryptoList) {
+     cryptoCards.add(
+       CryptoCard(
+         cryptoCurrency: crypto,
+         selectedCurrency: selectedCurrency,
+         value: isWaiting ? '?' : coinValues[crypto],
+       ),
+     );
+   }
+   return Column(
+     crossAxisAlignment: CrossAxisAlignment.stretch,
+     children: cryptoCards,
+   );
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -87,29 +111,56 @@ class _PriceScreenState extends State<PriceScreen> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+          //Widget 関数化
           //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
           //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $value $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          // CryptoCard(selectedCurrency);
+          // CryptoCard(selectedcurrency);
+          // CryptoCard(selectedcurrency);
+
+          CryptoCard(
+            cryptoCurrency: 'BTC',
+            //7. Finally, we use a ternary operator to check if we are waiting and if so, we'll display a '?' otherwise we'll show the actual price data.
+            value: isWaiting ? '?' : coinValues['BTC'],
+            selectedCurrency: selectedCurrency,
           ),
+          CryptoCard(
+            cryptoCurrency: 'ETH',
+            value: isWaiting ? '?' : coinValues['ETH'],
+            selectedCurrency: selectedCurrency,
+          ),
+          CryptoCard(
+            cryptoCurrency: 'LTC',
+            value: isWaiting ? '?' : coinValues['LTC'],
+            selectedCurrency: selectedCurrency,
+          ),
+
+
+
+          //TODO 1で以下リファクタリング
+          // Padding(
+          //   padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          //   child: Card(
+          //     color: Colors.lightBlueAccent,
+          //     elevation: 5.0,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: Padding(
+          //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          //       child: Text(
+          //         '1 BTC = $value $selectedCurrency',
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //           fontSize: 20.0,
+          //           color: Colors.white,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          //TODO 1
+
           Container(
             height: 150.0,
             alignment: Alignment.center,
@@ -122,3 +173,47 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 }
+
+//TODO 1リファクタリング
+//1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+class CryptoCard extends StatelessWidget {
+  //2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+  //TODO 2 コンストラクタ
+  const CryptoCard({
+    this.value,
+    this.selectedCurrency,
+    this.cryptoCurrency,
+  });
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+  //TODO 2
+
+  //TODO 3 Paddingを関数呼び出しする
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
